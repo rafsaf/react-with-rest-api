ff7014b39cebd77b629ae5a9d45f0b8366c3a5d6
rafsaf <rafal.safin12@gmail.com>
2020-09-14 19:42:01 +0200

    Updates

---
+++ precache-manifest.10988041d7b6e903dfb690fa89480964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f3cf65c549aa60c8b8b56d65df3e75",
+    "url": "/react-with-rest-api/index.html"
+  },
+  {
+    "revision": "6397f6bfb3df37d422fa",
+    "url": "/react-with-rest-api/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "fb16635f3fdfea9ecda3",
+    "url": "/react-with-rest-api/static/js/2.ab60dda3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-with-rest-api/static/js/2.ab60dda3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6397f6bfb3df37d422fa",
+    "url": "/react-with-rest-api/static/js/main.3d179a9a.chunk.js"
+  },
+  {
+    "revision": "76bf5ce8d8f361ff37da",
+    "url": "/react-with-rest-api/static/js/runtime-main.af0a0e63.js"
+  }
+]);
